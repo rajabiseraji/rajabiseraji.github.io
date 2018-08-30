8362100b2fa9df016e42231abe6f9851784da3c5
Mohammad Rajabi <rajabiseraji@aut.ac.ir>
2018-08-30 16:05:58 +0430

    feat(MenuIcons): adds the svg icons from FlatIcon to the menus

--- layouts/default.vue
+++ layouts/default.vue
@@ -14,10 +14,14 @@
           :key="i"
           v-for="(item, i) in items"
           exact
+          :avatar="item.image ? true : false"
         >
-          <v-list-tile-action>
+          <v-list-tile-action v-if="!item.image ? true : false">
             <v-icon v-html="item.icon"></v-icon>
           </v-list-tile-action>
+          <v-list-tile-avatar size="32" v-else tile>
+            <img :src="item.image">
+          </v-list-tile-avatar>
           <v-list-tile-content>
             <v-list-tile-title v-text="item.title"></v-list-tile-title>
           </v-list-tile-content>
@@ -38,13 +42,13 @@
       <v-container fill-height>
         <v-layout row align-center fill-height>
           <v-flex xs12 sm10>
-            <span>Coded with &#9825; by rajabiseraji - Using Nuxt and Vuetify</span>
+            <span class="caption">Coded with &#9825; by rajabiseraji - Using Nuxt and Vuetify</span>
           </v-flex>
           <v-flex>
           <v-spacer></v-spacer>
           </v-flex>
           <v-flex class="text-xs-right" xs12 sm2>
-            <span>&copy; 2018</span>
+            <span class="caption">&copy; 2018</span>
           </v-flex>
         </v-layout>
       </v-container>
@@ -59,13 +63,13 @@
         bottomNav: 'recent',
         drawer: false,
         items: [
-          { icon: 'apps', title: 'Overview', to: '/' },
-          { icon: 'info', title: 'About me', to: '/about' },
-          { icon: 'school', title: 'Education', to: '/education' },
-          { icon: 'work', title: 'Experience', to: '/experience' },
-          { icon: 'color_lens', title: 'Skills', to: '/skills' },
-          { icon: 'attach_money', title: 'Honours and Awards', to: '/awards' },
-          { icon: 'contacts', title: 'Contact', to: '/contact' }
+          { icon: 'apps', title: 'Overview', to: '/', image: '/menu.svg' },
+          { icon: 'info', title: 'About me', to: '/about', image: '/information.svg' },
+          { icon: 'school', title: 'Education', to: '/education', image: '/mortarboard.svg' },
+          { icon: 'work', title: 'Experience', to: '/experience', image: '/briefcase.svg' },
+          { icon: 'color_lens', title: 'Skills', to: '/skills', image: '/skills.svg' },
+          { icon: 'attach_money', title: 'Honours and Awards', to: '/awards', image: '/cup.svg' },
+          { icon: 'contacts', title: 'Contact', to: '/contact', image: '/contact.svg' }
         ],
         miniVariant: false,
         title: 'Mohammad Rajabi Seraji'
