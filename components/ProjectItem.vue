3b2e7daf43392eae46ecfbbd627a2fe8b6bc7b64
Mohammad Rajabi <rajabiseraji@aut.ac.ir>
2018-09-01 12:58:49 +0430

    feat(Project and Research): adds these shits dg

---
+++ components/ProjectItem.vue
@@ -0,0 +1,50 @@
+<template>
+<v-layout row>
+  <v-card class="mr-2 mb-2" width="100%">
+    <v-layout row wrap class="w-100">
+      <v-flex class="img-section" xs12 sm4 justify-center align-center>
+        <div>
+          <img :src="projectItem.logo" height="200px" />
+        </div>
+      </v-flex>
+      <v-flex xs12 sm8>
+        <v-card-title>
+          <div>
+            <span class="headline">{{projectItem.title}}</span><br>
+            <span class="subheading">{{projectItem.location}}</span>
+            <div class="grey--text caption">{{projectItem.duration}}</div><br>
+          </div>
+        </v-card-title>
+        <v-card-text>
+          <p>{{projectItem.description}}</p>
+        </v-card-text>
+        <v-card-actions>
+          <v-btn flat dark class="black" :href="projectItem.link">
+            <v-avatar size="20" class="mr-2">
+              <img src="/github.svg" alt="g">
+            </v-avatar>
+            Github Repository
+          </v-btn>
+        </v-card-actions>
+      </v-flex>
+    </v-layout>
+  </v-card>
+</v-layout>
+</template>
+
+<script>
+export default {
+  props: {
+    projectItem: Object
+  }
+}
+</script>
+
+<style scoped>
+  .my-card {
+    width: 300px;
+  }
+  .img-section {
+    display: flex;
+  }
+</style>
---
+++ pages/projects.vue
@@ -0,0 +1,54 @@
+<template>
+    <v-container fluid fill-height class="my-container">
+      <v-layout row wrap fill-height>
+        <v-flex xs12>
+            <div class="display-2 font-weight-thin mb-3">
+                Projects and Research
+            </div>
+            <v-divider></v-divider>
+        </v-flex>
+        <v-flex class="mt-4" xs12>
+            <div class="headline font-weight-thin mb-1">
+                Research
+            </div>
+            <v-divider></v-divider>
+        </v-flex>
+        <v-flex xs12 class="mt-2">
+          <ProjectItem :projectItem="projectItem"
+                        v-for="projectItem in projectRecords" 
+                        :key="projectItem.id" />
+        </v-flex>
+        <v-flex xs12>
+            <div class="headline font-weight-thin mb-1">
+                Projects
+            </div>
+            <v-divider></v-divider>
+        </v-flex>
+        <v-flex xs12 class="mt-2">
+          <ProjectItem :projectItem="researchItem"
+                        v-for="researchItem in researchRecords" 
+                        :key="researchItem.id" />
+        </v-flex>
+      </v-layout>
+    </v-container>
+</template>
+
+<script>
+import ProjectItem from '~/components/ProjectItem'
+import records from '~/static/records.json'
+export default {
+  components: {
+    ProjectItem
+  },
+  data () {
+    return {
+      projectRecords: records.Projects.Research,
+      researchRecords: records.Projects.Projects
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
