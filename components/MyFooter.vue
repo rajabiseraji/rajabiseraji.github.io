5b71c524ec64c7829bde6dfa71483feaf6ecdc9e
Mohammad Rajabi <rajabiseraji@aut.ac.ir>
2018-09-16 13:56:02 +0430

    feat(MyFooter): adds the footer

---
+++ components/MyFooter.vue
@@ -0,0 +1,39 @@
+<template>
+  <v-footer
+  dark
+  height="auto"
+  >
+    <v-card
+      flat
+      tile
+      class="indigo lighten-1 white--text text-xs-center"
+    >
+      <v-card-text class="white--text pt-0">
+        This website was built from scratch using <a href="https://vuejs.org/">Vue</a>, <a href="https://nuxtjs.org/">Nuxt</a> and <a href="https://vuetifyjs.com/en/">Vuetify</a>
+        and is available publicly on <a href="https://github.com/rajabiseraji/rajabiseraji.github.io">GitHub<sup>&copy;</sup></a>. Star it if you enjoy :) and also feel free to 
+        use the template, bend it in anyway you want! ^_^
+      </v-card-text>
+
+      <v-divider></v-divider>
+      <v-card-text class="white--text pt-0">
+        <div class="font-weight-bold heading">Disclaimer</div>
+        <v-divider></v-divider>
+        <div>Icons made by <a href="https://www.flaticon.com/authors/pixel-perfect" title="Pixel perfect">Pixel perfect</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a> is licensed by <a href="http://creativecommons.org/licenses/by/3.0/" title="Creative Commons BY 3.0" target="_blank">CC 3.0 BY</a></div>
+      </v-card-text>
+
+      <v-card-text class="white--text">
+        &copy;2018 — <strong>Built with <span class="red">&#9829;</span> using Vue, Nuxt and Vuetify</strong>
+      </v-card-text>
+    </v-card>
+  </v-footer>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
