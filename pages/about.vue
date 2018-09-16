40ae2c10baa4f2136366e1d556f18d6d0a30190a
Mohammad Rajabi <rajabiseraji@aut.ac.ir>
2018-09-16 14:58:29 +0430

    fix(about): adds about site page

--- pages/about.vue
+++ pages/about.vue
@@ -1,5 +1,25 @@
 <template>
-    
+  <v-layout row wrap fill-height>
+    <v-flex xs12>
+        <div class="display-2 font-weight-thin mb-3">
+            About This Site
+        </div>
+        <v-divider></v-divider>
+        <div class="pt-0 mt-3 body-2">
+          This website was built from scratch using <a href="https://vuejs.org/">Vue</a>, <a href="https://nuxtjs.org/">Nuxt</a> and <a href="https://vuetifyjs.com/en/">Vuetify</a>
+          and is available publicly on <a href="https://github.com/rajabiseraji/rajabiseraji.github.io">GitHub<sup>&copy;</sup></a>. Star it if you enjoy :) and also feel free to 
+          use the template, bend it in anyway you want! ^_^
+        </div>
+
+        <div class="pt-0 mt-2 body-2">
+          <div class="font-weight-bold title mb-1">Disclaimer</div>
+          <div>Icons made by <a href="https://www.flaticon.com/authors/pixel-perfect" title="Pixel perfect">Pixel perfect</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a> is licensed by <a href="http://creativecommons.org/licenses/by/3.0/" title="Creative Commons BY 3.0" target="_blank">CC 3.0 BY</a></div>
+        </div>
+    </v-flex>
+    <v-flex xs12>
+      
+    </v-flex>
+  </v-layout>
 </template>
 
 <script>
