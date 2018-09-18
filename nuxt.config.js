b77d1596286afbb4b48ed3ee6b043598246808e1
Mohammad Rajabi <rajabiseraji@aut.ac.ir>
2018-09-18 13:31:34 +0430

    fix(nuxt.config): fixes title and description of the site

--- nuxt.config.js
+++ nuxt.config.js
@@ -6,11 +6,11 @@ module.exports = {
   ** Headers of the page
   */
   head: {
-    title: 'rajabiseraji.github.io',
+    title: 'Mohammad Rajabi Seraji Homepage',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: 'RMohammad Rajabi Seraji Homepage' }
+      { hid: 'description', name: 'description', content: 'Mohammad Rajabi Seraji Homepage' }
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
