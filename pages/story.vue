a354f46454986391eb4e363ca88f36466f73a1dd
Mohammad Rajabi <rajabiseraji@aut.ac.ir>
2018-09-18 12:15:11 +0430

    fix(story): adds the story layout to the page

--- pages/story.vue
+++ pages/story.vue
@@ -5,15 +5,18 @@
             My Story
         </div>
         <v-divider></v-divider>
-        <div class="pt-0 mt-3 body-2">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum
-        </div>  
-        <div class="pt-0 mt-3 body-2">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum
-        </div>  
-        <div class="pt-0 mt-3 body-2">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum
-        </div>  
+        <v-layout row wrap>
+          <v-flex xs12 v-for="(storyPart, index) in storyParts" :key="storyPart.id" class="pt-0 mt-3 body-2">
+            <v-layout row wrap :reverse="index%2 === 0">
+              <v-flex d-flex justify-center align-center xs12 sm3 v-if="index > 2 && storyPart.photo" class="story-image my-2">
+                <img height="150px" :src="storyPart.photo" alt="image">
+              </v-flex>
+              <v-flex xs12 sm9 v-if="index > 2" d-flex justify-center align-center class="story-text">{{storyPart.text}}</v-flex>
+              <v-flex xs12 v-else class="font-weight-thin verses">{{storyPart.text}}</v-flex>
+            </v-layout>
+            <div class="mb-5" v-if="index === 2"></div>
+          </v-flex>  
+        </v-layout>
     </v-flex>
     <v-flex xs12>
       
@@ -22,11 +25,29 @@
 </template>
 
 <script>
+import records from '~/static/records.json'
 export default {
-
+  data () {
+    return {
+      storyParts: records.Story
+    }
+  }
 }
 </script>
 
-<style>
-
+<style scoped>
+.story-image {
+  display: flex;
+  width: 100%;
+  justify-content: center;
+  align-items: center;
+}
+.verses {
+  text-align: center;
+  line-height: 0.7;
+  font-size: 1.5rem;
+}
+.story-text {
+  text-align: justify;
+}
 </style>
